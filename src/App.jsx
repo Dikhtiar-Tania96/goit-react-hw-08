fa96c1eff1dfe6618dc40a74fdfdcb0542f6abac
Dikhtiar Tania <you@example.com>
2024-07-08 00:10:35 +0300

    fix code

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
-import { useEffect } from 'react';
 import './App.css';
-import { useDispatch } from 'react-redux';
-import { fetchContacts } from './redux/contactsOps';
-import ContactForm from './components/ContactForm/ContactForm'
-import SearchBox from './components/SearchBox/SearchBox'
-import ContactList from './components/ContactList/ContactList'
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
+  
 
   return (
     <div>
-      <h1>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
-      <ContactList />
+     <Navigation/>
     </div>
   );
 }
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom";
+
+
+const Navigation = () => {
+    return (
+<nav>
+    <NavLink>Home</NavLink>
+</nav>
+    )
+}
+
+export default Navigation
